a56aa4d4991c982b498172ca161583dc0db51918
Bas Wegh <bas.wegh@kit.edu>
2016-10-13 09:04:40 +0200

    print version during build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,15 @@ PATH_TO_SCRIPT=`readlink -f ${0}`
 PATH_TO_FOLDER=`dirname "$PATH_TO_SCRIPT"`
 
 if [ "x$GO" == "x" ]; then
-    echo "go missing, please install go 1.x"
+    echo "go missing, please install go 1.5 or newer"
     exit 1
 fi
 
+VERSION=`go version`
 GOPATH=`cd "${PATH_TO_FOLDER}/.." && pwd -P`
+echo " "
+echo "running the build with '$VERSION', please include in issue reports"
+echo " "
 export "GOPATH=${GOPATH}"
 echo "fetiching:"
 echo -n "  kingpin ... "
