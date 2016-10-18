7cc5d1911d594164fb89ed8fc50001f2acede833
Bas Wegh <bas.wegh@kit.edu>
2016-10-18 14:02:11 +0200

    get version from compiled orchent directly

--- scripts/package_config.sh
+++ scripts/package_config.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-export VERSION=0.1.0
+export VERSION="$(./orchent --version 2>&1 )"
 export RPM_DIR=`pwd`/packaging/rpm/orchent
+export DEB_DIR=`pwd`/packaging/deb/orchent
+
+
+echo "exporting variables"
+echo "  VERSION=$VERSION"
+echo "  RPM_DIR=$RPM_DIR"
+echo "  DEB_DIR=$DEB_DIR"
