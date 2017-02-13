8c42d4f254794c7616dcadfd924e7179be170a8e
Bas Wegh <bas.wegh@kit.edu>
2017-02-13 14:34:02 +0100

    use tools like wattson

---
+++ utils/build-pkg.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+CURRENT_DIR=`pwd`
+cd `dirname $0`
+cd ..
+SOURCE_DIR=`pwd`
+VERSION=`git describe --tags`
+source ./utils/prepare_system.sh
+
+# expecting to be in $GOPATH/src/github.com/indigo-dc/orchent
+mkdir -p $GOPATH/src/github.com/indigo-dc/orchent
+cd $GOPATH/src/github.com/indigo-dc/orchent
+pwd
+cp -v $SOURCE_DIR/* .
+glide install
+cp -r vendor/* "$GOPATH/src"
+rm -fr pkg-build/*
+mkdir -p pkg-build/amd64
+GOOS=linux GOARCH=amd64 go build -o build/amd64/orchent orchent.go
+case $DISTRIBUTION in
+    debian)
+        GO_BIN="go-bin-deb"
+        PKG="orchent-$VERSION-amd64.deb"
+        PFLAG="-w"
+        ;;
+    centos)
+        GO_BIN="go-bin-rpm"
+        PKG="orchent-$VERSION.el7.centos.x86_64.rpm"
+        PFLAG="-b"
+        ;;
+esac
+$GO_BIN generate -a amd64 --version ${VERSION} ${PFLAG} pkg-build/amd64 -o ${PKG}
+
+echo " "
+echo " "
+echo " *** DONE ***"
---
+++ utils/prepare_system.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+DISTRIBUTION_NAME=`cat /etc/os-release | grep PRETTY_NAME`
+CURRENT_DIR=`pwd`
+cd `dirname $0`
+UTILS_DIR=`pwd`
+
+DISTRIBUTION="unknown"
+case "$DISTRIBUTION_NAME" in
+    *Debian*)
+        DISTRIBUTION="debian"
+        ;;
+    *Ubuntu*)
+        DISTRIBUTION="debian"
+        ;;
+    *CentOS*)
+        DISTRIBUTION="centos"
+        ;;
+esac
+export DISTRIBUTION="$DISTRIBUTION"
+echo "preparing the system ..."
+echo "distribution: $DISTRIBUTION"
+echo "utils dir: $UTILS_DIR"
+echo "current dir: $CURRENT_DIR"
+if [ "$DISTRIBUTION" = "unknown" ]; then
+    echo "ERROR: unknown distribution"
+    exit 1
+fi
+
+echo " "
+echo " "
+echo "*** INSTALLING PACKAGES ***"
+cd $UTILS_DIR
+case "$DISTRIBUTION" in
+    debian)
+        ./debian_install_packages.sh
+        ;;
+    centos)
+        ./centos_install_packages.sh
+        ;;
+esac
+
+echo " "
+echo " "
+echo "*** creating go environment ***"
+cd $UTILS_DIR/../..
+rm -rf orchent_build_env
+mkdir orchent_build_env
+curl -O https://storage.googleapis.com/golang/go1.6.4.linux-amd64.tar.gz
+tar -xzf go1.6.4.linux-amd64.tar.gz
+sudo mv go /usr/local
+export PATH="/usr/local/go/bin:$PATH"
+
+cd orchent_build_env
+case "$DISTRIBUTION" in
+    debian)
+        mkdir deb
+        cd deb
+        ;;
+    centos)
+        mkdir rpm
+        cd rpm
+        ;;
+esac
+export GOPATH=`pwd`
+export PATH="$GOPATH/bin:$PATH"
+GO_VERSION=`go version`
+echo "GOPATH: $GOPATH"
+echo "PATH: $PATH"
+echo "GO VERSION: $GO_VERSION"
+echo " "
+
+
+rm -rf $GOPATH
+mkdir -p $GOPATH
+cd $GOPATH
+mkdir bin
+mkdir pkg
+mkdir src
+
+echo " "
+echo " "
+echo "*** installing glide ***"
+curl https://glide.sh/get | sh
+
+
+echo " "
+echo " "
+echo "*** installing build utils ***"
+mkdir -p src/github.com/indigo-dc/orchent
+mkdir -p src/github.com/mh-cbon
+cd src/github.com/mh-cbon
+case "$DISTRIBUTION" in
+    debian)
+        git clone https://github.com/mh-cbon/go-bin-deb.git
+        cd go-bin-deb
+        git checkout 0.0.16
+        ;;
+    ubuntu)
+        git clone https://github.com/mh-cbon/go-bin-deb.git
+        cd go-bin-deb
+        git checkout 0.0.16
+        ;;
+    centos)
+        git clone https://github.com/mh-cbon/go-bin-rpm.git
+        cd go-bin-rpm
+        git checkout 0.0.15
+        ;;
+esac
+glide install
+cp -r vendor/* "$GOPATH/src"
+go install
+cd ..
+git clone https://github.com/mh-cbon/changelog.git
+cd changelog
+git checkout 0.0.24
+glide install
+cp -r vendor/* "$GOPATH/src"
+go install
+
+echo " "
+echo " "
+echo "*** SYSTEM SETUP DONE ***"
+echo " "
