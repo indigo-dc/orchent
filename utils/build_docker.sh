de43d36a24fdb7176c1d8e133f03b2dfa9656972
Bas Wegh <bas.wegh@kit.edu>
2017-02-24 16:10:04 +0100

    change build command

--- utils/build_docker.sh
+++ utils/build_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+GO=`which go`
+if [ "x$GO" == "x" ]; then
+    echo "go missing, please install go 1.5 or newer"
+    exit 1
+fi
 PATH_TO_SCRIPT=`readlink -f ${0}`
 PATH_TO_FOLDER=`dirname "$PATH_TO_SCRIPT"`
 PATH_TO_REPO=`cd "${PATH_TO_FOLDER}/.." && pwd -P`
@@ -8,11 +13,28 @@ DOCKERFILE="$PATH_TO_FOLDER/Dockerfile"
 ORCHENT="$PATH_TO_REPO/orchent"
 
 cd $REPO_PATH
-pwd
 echo " "
 echo " building orchent ..."
-./utils/compile.sh
+
+VERSION=`go version`
+GOPATH=`cd "${PATH_TO_FOLDER}/.." && pwd -P`
+
 echo " "
+echo "running the build with '$VERSION', please include in issue reports"
+echo " "
+export "GOPATH=${GOPATH}"
+echo "fetiching:"
+echo -n "  kingpin ... "
+go get gopkg.in/alecthomas/kingpin.v2
+echo "done"
+echo -n "  sling ... "
+go get github.com/dghubble/sling
+echo "done"
+echo -n "building orchent ... "
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix -v -o orchent ${GOPATH}/orchent.go
+echo "done"
+
+echo "building docker ... "
 mkdir -p /tmp/orchent_docker/
 cp $DOCKERFILE /tmp/orchent_docker/
 cp $ORCHENT /tmp/orchent_docker/
