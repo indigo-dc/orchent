acede6ceddaf424f6c3a970853704734d27991b2
Bas Wegh <bas.wegh@kit.edu>
2017-03-01 07:33:35 +0100

    use realpath instead of readling, should work on mac

--- utils/build_docker.sh
+++ utils/build_docker.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 GO=`which go`
+REALPATH=`which realpath`
 if [ "x$GO" == "x" ]; then
     echo "go missing, please install go 1.5 or newer"
     exit 1
 fi
-PATH_TO_SCRIPT=`readlink -f ${0}`
+
+if [ "x$REALPATH" == "x" ]; then
+    echo "realpath missing, please install it"
+    exit 1
+fi
+
+PATH_TO_SCRIPT=`realpath ${0}`
 PATH_TO_FOLDER=`dirname "$PATH_TO_SCRIPT"`
 PATH_TO_REPO=`cd "${PATH_TO_FOLDER}/.." && pwd -P`
 
@@ -31,7 +38,7 @@ echo -n "  sling ... "
 go get github.com/dghubble/sling
 echo "done"
 echo -n "building orchent ... "
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix -v -o orchent ${GOPATH}/orchent.go
+CGO_ENABLED=0 GOOS=linux go build -a -v -o orchent ${GOPATH}/orchent.go
 echo "done"
 
 echo "building docker ... "
