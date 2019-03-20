93a29444761e831fee5266b5abee45cc536e33c8
zachmann <gabriel.zachmann@kit.edu>
2019-03-20 15:52:07 +0100

    fix package building

--- utils/build_docker.sh
+++ utils/build_docker.sh
@@ -46,6 +46,9 @@ echo "done"
 echo -n "  go-config ... "
 go get github.com/zpatrick/go-config
 echo "done"
+echo -n "  liboidcagent ... "
+go get github.com/zachmann/liboidcagent-go/liboidcagent
+echo "done"
 echo -n "building orchent ... "
 CGO_ENABLED=0 GOOS=linux go build -a -v -o $ORCHENT ${GOPATH}/orchent.go
 echo "done"
